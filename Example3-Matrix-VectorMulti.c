4e9b2fa6bf279ce67fcb8b9fe11336a409b4af43
Mira Qiu <qmm0607@gmail.com>
2018-11-30 16:50:28 -0500

    Update Example3-Matrix-VectorMulti.c

--- Example3-Matrix-VectorMulti.c
+++ Example3-Matrix-VectorMulti.c
@@ -1,4 +1,56 @@
-//File name: a.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define m 6		//Number of rows
+#define n 5		//Number of columns
+
+int** a;
+int *x, *y;
+int thread_count = 3; //m must be divisible by this number
+
+void read(int* a[], int x[]);
+void *Pth_mat_vect(void* rank);
+void print(int* a[], int x[], int y[]);
+
+int main(){
+  long thread;
+  pthread_t* thread_handles;
+  a=(int**) malloc(m*sizeof(int*));
+  x = malloc(n * sizeof(int));
+  y = malloc(n * sizeof(int));
+  read(a, x);
+  thread_handles = malloc(thread_count*sizeof(pthread_t));
+  for (thread = 0; thread < thread_count; thread++)
+    pthread_create(&thread_handles[thread], NULL, Pth_mat_vect, (void*) thread);
+
+  for (thread = 0; thread < thread_count; thread++)
+    pthread_join(thread_handles[thread], NULL);
+
+  print(a, x, y);
+
+  free(thread_handles);
+
+  return 0;
+}
+
+void *Pth_mat_vect(void* rank){
+  long my_rank = (long) rank;
+  int i, j; 
+  int local_m = m/thread_count; 
+  int my_first_row = my_rank*local_m;
+  int my_last_row = my_first_row + local_m;
+  for (i = my_first_row; i < my_last_row; i++){
+    y[i] = 0;
+    for (j = 0; j < n; j++)
+      y[i] += a[i][j] * x[j];
+  }
+  return NULL;
+}
+
+
+
+//create matrix, and vector
 #include <stdio.h>
 #include <stdlib.h>
 
